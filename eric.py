469b56767fb5f8d5adfe07440892f173e94f8d0d
alanmaydwell <alan.maydwell@legalaid.gsi.gov.uk>
2018-01-11 08:11:35 +0000

    still WIP but currently works

--- eric.py
+++ eric.py
@@ -153,7 +153,7 @@ class Eric(object):
         # Wait for "please wait" to go
         WebDriverWait(driver,20).until(lambda driver: not self.check_page_blocked())
 
-    def report_list(self):
+    def report_list_items(self):
         """Finds which reports are listed
         Should either be 0 or the 4 standard reports
 
@@ -162,21 +162,29 @@ class Eric(object):
         "Family mediation financial statement"
         "Financial statement summary"
 
+        Returns search result message, list of report names,
+        eg. "4 reports found for 2N875V", [u'Civil financial statement', u'Criminal financial statement', u'Family mediation financial statement', u'Financial statement summary']
+
         """
         driver = self.driver
-        # If report search successful, there's a div on the page
-        # containing the results, otherwise it's abscent
 
-        try:
-            div = driver.find_element_by_class_name("tableBoxIndHalf2")
-        except NoSuchElementException as e:
-            pass
-        else:
-            reports = [e.text for e in div.find_elements_by_tag_name("a")]
+        # No convenient locators for these items. Also response message
+        # in different location if search unsuccessful
+        # If report search successful, there's a div on the page
+
+        # If search was successful there's a "tableBoxIndHalf2" div
+        results_div = driver.find_elements_by_class_name("tableBoxIndHalf2")
+        # Search result has reports
+        if results_div:
+            div = results_div[0]
+            report_names = [e.text for e in div.find_elements_by_tag_name("a")]
             message = div.find_element_by_tag_name("p").text
-            print reports
-            print message
-            print ""
+        # Search result has no reports
+        else:
+            report_names = []
+            message = driver.find_element_by_tag_name("ul").text
+
+        return message, report_names
 
 
     def select_report(self, report_no=0):
@@ -218,6 +226,26 @@ class Eric(object):
         # Wait for "Please Wait to go"
         WebDriverWait(driver,20).until(lambda driver: not self.check_page_blocked())
 
+    def examine_report_details(self):
+        """Examine details from already open report
+        Not finished!
+        """
+        driver = self.driver
+        # Report is in an iframe
+        report_frame = driver.find_element_by_id("reportContent")
+        driver.switch_to_frame(report_frame)
+        # Results are in four tables within (Summary, Detail, Additional, Payments)
+        tables = driver.find_elements_by_tag_name("table")
+
+        #Details from 1st table
+        print "Report Heading Fnfo"
+        summary_rows = tables[0].find_elements_by_tag_name("tr")
+        for row in summary_rows:
+            cells = row.find_elements_by_tag_name("td")
+            print " ".join([e.text for e in cells])
+
+        #Leave the iframe
+        driver.switch_to_default_content()
 
     def close_report(self):
         """
@@ -245,18 +273,36 @@ if __name__ == "__main__":
     url = "https://syssso10.laadev.co.uk/"
     username = "pear-s"
     password = getpass.getpass(prompt="Password:")
-    go = Eric()
-    result = go.login(username, password, url)
-    print result
-    go.open_eric()
-    for things in ["0G934M", "Mangle", "2N875V"]:
-        go.search(things)
-        go.report_list()
-    """
-    for report_no in [0, 1, 2, 3]:
-        print fn_timer(go.select_report, report_no)
+    test = Eric()
 
-    go.view_report()
-    go.close_report()
-    """
+    # Login to Portal (not timed)
+    login_result = test.login(username, password, url)
+    print "\nPortal login result:", login_result
+
+    # Open CCR from Portal Link
+    launch_time = fn_timer(test.open_eric)
+    print url
+    print "CCR launch time:", launch_time
+
+    # Perform Search in CCR
+    search_time = fn_timer(test.search, "0G934M")
+    print "Search time:", search_time
+
+    # Show which reports were found (not timed)
+    message, report_list = test.report_list_items()
+    print message
+
+    # Select each report in turn and view it
+    for report_no in [0, 1, 2, 3]:
+        print "\nTimings for report:", report_no
+        # Select the report
+
+        print "Select:", fn_timer(test.select_report, report_no)
+        # View the selected report
+        print "View:", fn_timer(test.view_report)
+        #Examine report details - not complete
+        ##test.examine_report_details()
+        # Close the report (not timed)
+        test.close_report()
+
     print "Finished"
